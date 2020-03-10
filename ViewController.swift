3cc2b408db01e77831ac31d827ddb3d769fa32df
Aliveng <aliveng@yandex.ru>
2020-03-10 19:06:53 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightsApp
+//
+//  Created by Татьяна Севостьянова on 10.03.2020.
+//  Copyright © 2020 Татьяна Севостьянова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
